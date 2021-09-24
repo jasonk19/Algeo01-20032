d428249b712bb4e108d6d0594e2963a16efb5f43
jask-19 <16520214@std.stei.itb.ac.id>
2021-09-24 21:53:45 +0700

    fix interpolasi polinom

--- src/Interpolation.java
+++ src/Interpolation.java
@@ -24,69 +24,72 @@ public class Interpolation {
  
   void convertToMatAug(matriks titik) {
 
-    titik.NeffK = titik.NeffK+2;
+    titik.NeffK = titik.NeffK+(titik.NeffB-1);
 
     float[] x = new float[titik.NeffB];
     float[] y = new float[titik.NeffB];
-    float[] xKuadrat = new float[titik.NeffB];
 
     int i,j;
     for (i = 0; i < titik.NeffB; i++) {
       x[i] = titik.Mat[i][0];
       y[i] = titik.Mat[i][1];
-      xKuadrat[i] = titik.Mat[i][0] * titik.Mat[i][0];
     }
 
     for (i = 0; i < titik.NeffB; i++) {
       for (j = 0; j < titik.NeffK; j++) {
         if (j == 0) {
           titik.Mat[i][j] = 1;
-        }
-        if (j == 1) {
-          titik.Mat[i][j] = x[i];
-        }
-        if (j == 2) {
-          titik.Mat[i][j] = xKuadrat[i];
-        }
-        if (j == 3) {
+        } else if (j == titik.NeffK-1) {
           titik.Mat[i][j] = y[i];
+        } else {
+          titik.Mat[i][j] = (float) Math.pow((double) x[i], (double) j);
         }
       }
     }
-
-    titik.displayMatriks();
+    // titik.displayMatriks();
+    System.out.println();
   }
 
   void displayInterpolasi(matriks titik, float x) {
     int i,j;
+
+    System.out.println("System persamaan lanjar yang terbentuk: ");
     // display matriks augmented dari titik
-    for (i = 0; i < titik.NeffB; i++) {
-      for (j = 0; j < titik.NeffK; j++) {
-        if (j == 0) {
-          System.out.print("a0");
-          System.out.print(" + ");
+    if (titik.NeffB == 1) {
+      System.out.print("a0");
+      System.out.print(" = ");
+      System.out.println(titik.Mat[0][1]);
+    } else {
+      for (i = 0; i < titik.NeffB; i++) {
+        for (j = 0; j < titik.NeffK; j++) {
+          if (j == 0) {
+            System.out.print("a0");
+            System.out.print(" + ");
+          } else if (j == titik.NeffK-2) {
+            System.out.print(titik.Mat[i][j]);
+            System.out.print("a");
+            System.out.print(j);
+            System.out.print(" = ");
+          } else if (j == titik.NeffK-1) {
+            System.out.print(titik.Mat[i][j]);
+          } else {
+            System.out.print(titik.Mat[i][j]);
+            System.out.print("a");
+            System.out.print(j);
+            System.out.print(" + ");
+          }
         }
-        if (j == 1) {
-          System.out.print(titik.Mat[i][j]);
-          System.out.print("a1");
-          System.out.print(" + ");
-        }
-        if(j == 2) {
-          System.out.print(titik.Mat[i][j]);
-          System.out.print("a2");
-          System.out.print(" = ");
-        }
-        if (j == 3) {
-          System.out.print(titik.Mat[i][j]);
-        }
+        System.out.println();
       }
-      System.out.println();
     }
+
+    System.out.println();
 
     System.out.print("Hasil taksiran dari nilai x = ");
     System.out.print(x);
     System.out.println(" adalah: ");
   }
+
 
 
 }
