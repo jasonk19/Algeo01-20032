9f689036b62f55240a52be37e2b526ebf63f0154
jask-19 <16520214@std.stei.itb.ac.id>
2021-09-24 15:08:19 +0700

    add interpolasi, baru input titik, belom convert titik to mat

---
+++ src/Interpolation.java
@@ -0,0 +1,74 @@
+public class Interpolation {
+
+
+  matriks readTitikIntepolation() {
+    matriks titik = new matriks();
+    int N;
+
+    System.out.println("Masukkan jumlah titik: ");
+    N = Main.scanner.nextInt();
+
+    System.out.println("Masukkan kombinasi titik: ");
+    titik.readMatriks(N,2);
+
+    return titik;
+
+  }
+
+  float readxTaksiran() {
+    float x;
+    System.out.println("Masukkan nilai x yang akan ditaksir: ");
+    x = Main.scanner.nextFloat();
+    return x;
+  }
+
+  // void displayTitik(matriks titik) {
+  //   titik.displayMatriks();
+  // }
+ 
+  matriks convertToMatAug() {
+    matriks matAug = new matriks();
+
+    int i,j;
+    for (i = 0; i < 4; i++) {
+      for (j = 0; j < 4; j++) {
+        matAug.Mat[i][j] = 1;
+      }
+    }
+
+    return matAug;
+  }
+
+  void displayInterpolasi(matriks matAug, float x) {
+    int i,j;
+    // display matriks augmented dari titik
+    for (i = 0; i < matAug.NeffB; i++) {
+      for (j = 0; j < matAug.NeffK; j++) {
+        if (j == 0) {
+          System.out.print("a0");
+          System.out.print(" + ");
+        }
+        if (j == 1) {
+          System.out.print(matAug.Mat[i][j]);
+          System.out.print("a1");
+          System.out.print(" + ");
+        }
+        if(j == 2) {
+          System.out.print(matAug.Mat[i][j]);
+          System.out.print("a2");
+          System.out.print(" = ");
+        }
+        if (j == 3) {
+          System.out.print(matAug.Mat[i][j]);
+        }
+      }
+      System.out.println();
+    }
+
+    System.out.print("Hasil taksiran dari nilai x = ");
+    System.out.print(x);
+    System.out.println(" adalah: ");
+  }
+
+
+}
